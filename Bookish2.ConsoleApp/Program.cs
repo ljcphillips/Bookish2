eda4fb58b415bd18ceda876401a1c3eb8c792a39
Lorraine Phillips <lorraine.phillips@softwire.com>
2018-09-11 13:58:18 +0100

    Renamed DataAccess Project

--- Bookish2.ConsoleApp/Program.cs
+++ Bookish2.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bookish2.DataAccess;
 
 namespace Bookish2.ConsoleApp
 {
@@ -10,6 +11,18 @@ namespace Bookish2.ConsoleApp
     {
         static void Main(string[] args)
         {
+            string SqlString = "SELECT TOP 100 [ItemID],[UserID],[ReturnDate],[BookID] FROM [Items]";
+            var ourItems = (List<DataAccess.Item>)db.Query<Item>(SqlString);
+            foreach (var Item in ourItems)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nItem ID: " + Item.ItemID.ToString());
+                Console.WriteLine("\nUser ID: " + Item.UserID.ToString());
+                Console.WriteLine("\nReturn Date: " + Item.ReturnDate.ToString());
+                Console.WriteLine("\nBook ID: " + Item.BookID.ToString());
+                Console.WriteLine(new string('*', 20));
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Bookish2.DataAccess/Data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Collections.Specialized;
+
+namespace Bookish.DataAccess
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public char ISBN { get; set; }
+        public char BookTitle { get; set; }
+        public char BookAuthor { get; set; }
+    }
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public int UserID { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int BookID { get; set; }
+    }
+
+    public class Connect
+    {
+        IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+    }
+}
